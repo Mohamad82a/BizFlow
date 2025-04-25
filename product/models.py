e5fc80f8ca5d32cb0ef572fcca2226a2393de1ab
Mohamad82a <mohamad2003abasi@gmail.com>
2025-04-25 16:09:01 +0330

    C2: products added via import_products.py from product_list.csv

--- product/models.py
+++ product/models.py
@@ -15,4 +15,7 @@ class Product(models.Model):
     inventory = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
