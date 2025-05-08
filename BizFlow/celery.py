5f2ea1a320ab788c9c998426fab9662a3600bde6
Mohamad82a <mohamad2003abasi@gmail.com>
2025-05-08 23:42:38 +0330

    C12: docker-compose.yml added to run the services and databases.

--- BizFlow/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bizflow.settings')
-
-app = Celery('bizflow')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- biz_ops/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BizOpsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'biz_ops'
