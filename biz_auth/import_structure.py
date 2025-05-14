90e0a95ba2586d09fa641be54474a34df0023464
Mohamad82a <mohamad2003abasi@gmail.com>
2025-05-14 14:43:37 +0330

    C18: company_structure.csv added and imported via import_structure.py.

--- biz_auth/import_structure.py
+++ biz_auth/import_structure.py
@@ -2,7 +2,7 @@ import os
 import django
 import csv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BizFlow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BizAuth.settings')
 django.setup()
 
 from account.models import Department, Role
@@ -12,11 +12,11 @@ with open('company_structure.csv', newline='', encoding='utf-8-sig') as csvfile:
     for row in reader:
         # print(row.keys())
         department_name = row['بخش']
-        department, _ = Department.objects.get_or_create(name=department_name)
+        department, _ = Department.objects.get_or_create(department=department_name)
 
         Role.objects.update_or_create(
             department=department,
-            name=row['عنوان شغلی'],
+            role=row['عنوان شغلی'],
             responsibility=row['شرح وظایف'],
         )
 print("Import done ✅")
